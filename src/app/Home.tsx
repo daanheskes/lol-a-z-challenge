2834f9dd60ec3ef89add2fc746f59df1d36cdb39
Daan Heskes <daanheskes@hotmail.com>
2025-12-10 09:31:14 +0100

    Changed method to not use Vercel Image Optimization (limit was exceeded)

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 
-// import Image from "next/image";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 // --- INTERFACE DEFINITIONS ---
@@ -151,7 +151,7 @@ export default function Home({ initialChampions, version }: HomeProps) {
                                 done ? "border-green-500 border-2" : "border-gray-400 border-0"
                             }`}
                         >
-                            <img
+                            <Image
                                 src={IMG_BASE_URL + champ.image.full}
                                 alt={champ.name}
                                 width={124}
@@ -201,7 +201,7 @@ function Modal({
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
                 <h2 className="text-xl font-bold mb-4">{champion.name} Progress</h2>
-                <img
+                <Image
                     src={IMG_BASE_URL + champion.image.full}
                     alt={champion.name}
                     width={64}
