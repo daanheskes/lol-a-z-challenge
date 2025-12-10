96175e3c346dff510eff5ccc96a0458b32e8dfce
Daan Heskes <daanheskes@hotmail.com>
2025-12-10 09:13:08 +0100

    test: bypass vercel image optimization

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -192,7 +192,7 @@ function Modal({
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
                 <h2 className="text-xl font-bold mb-4">{champion.name} Progress</h2>
-                <Image
+                <img
                     src={IMG_BASE_URL + champion.image.full}
                     alt={champion.name}
                     width={64}
