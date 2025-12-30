62cd82f3ac7c3a1dd9e8f4433d64ad9e7fbb815c
Daan Heskes <daanheskes@hotmail.com>
2025-12-30 09:35:25 +0100

    optimize images again

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: 'https',
