a0795cd36e0c589dc0be0058898b2d63a9594378
Daan Heskes <daanheskes@hotmail.com>
2026-06-10 09:36:31 +0200

    unoptimize images again, due to Vercel limits being constantly reached

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
